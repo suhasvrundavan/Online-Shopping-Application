e3ac9293fe19c371bac7a28345fb84bb14ccf814
suhasvrundavan <149283460+suhasvrundavan@users.noreply.github.com>
2023-10-29 21:05:51 +0530

    Add files via upload

---
+++ spring-boot-microservices/order-service/src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
@@ -0,0 +1,25 @@
+package com.programmingtechie.orderservice.controller;
+
+import com.programmingtechie.orderservice.dto.OrderRequest;
+import com.programmingtechie.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+@Slf4j
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        log.info("Placing Order");
+        return orderService.placeOrder(orderRequest);
+    }
+
+}
